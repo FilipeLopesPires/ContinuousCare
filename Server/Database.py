03b54c06f85421d30f62a4ece4fa11aba2623eb4
aspedrosa <aspedrosa@ua.pt>
2019-03-11 18:13:31 +0000

    Database proxy

---
+++ Server/Database.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import time
+
+import influxdb
+
+class Database:
+    def __init__(self):
+        self.client = influxdb.InfluxDBClient()
+        self.client.create_database("db")
+        self.client.switch_database("db")
+
+    def insert(self, data, lat, longi, clientId, deviceId):
+        points = []
+
+        for metric, val in data.items():
+            point = {
+                "measurement": metric,
+                "tags": {
+                    "clientId": clientId,
+                    "deviceId": deviceId,
+                },
+                "time": int(time.time()),
+                "fields": {
+                    "value": val,
+                    "lat": lat,
+                    "long": longi
+                }
+            }
+
+            points.append(point)
+
+        self.client.write_points(points)
