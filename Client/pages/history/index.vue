8d2ad58adbfe84251d676f8cd092007d00247211
FilipePires98 <filipesnetopires@ua.pt>
2019-03-11 15:19:11 +0000

    First HTTP request to Server, fully functional!

--- Client/pages/history/index.vue
+++ Client/pages/history/index.vue
@@ -1,12 +1,33 @@
 <template>
     <div class="history_page">
         <h1>History Page</h1>
+        <p> {{ jsonData }} </p>
     </div>
 </template>
 
 <script>
+import axios from "axios"
+
 export default {
-  layout: "client"
+    data() {
+        return {
+            //jsonData: this.loadExternalData()
+            jsonData: []
+        }
+    },
+    mounted() {
+        var self = this;
+        //axios.get("https://reqres.in/api/users?page=2")
+        axios.get("http://192.168.43.136:5000/pollution/lat=40/long=-8")
+        .then( function(response) {
+            console.log(response.data.data);
+            self.jsonData = response.data.data;
+        })
+        .catch( function(error) {
+            self.jsonData = "An error occurred.\n" + error;
+        });
+    },
+    layout: "client"
 }
 </script>
 
---
+++ Server/README.md
@@ -0,0 +1,11 @@
+# ContinuousCare
+
+## Build Setup 
+
+```sh
+$ python3 -m venv venv
+$ source venv/bin/activate
+$ pip3 install -r requirements.txt
+$ export FLASK_APP=REST.py
+$ python3 -m flask run
+```
