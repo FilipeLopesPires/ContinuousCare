9986f7ab2f60987af29cb5214ccab37f9bbb997c
joaoalegria <joao.p@ua.pt>
2019-04-01 15:39:38 +0100

    Updated Processing and Rest module, not tested yet, nor saving functions implemented

--- Server/Processor.py
+++ Server/Processor.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import string
+from random import *
 import threading
 from datetime import datetime, timedelta
 from database import *
@@ -10,63 +12,264 @@ Component responsable for doing all the information gathering and processing
 class Processor:
 
     def __init__(self):
+        self.supportedDevices=json.loads(open("config.json").read().replace("\n", "").replace("\t", "").strip())
         self.database=database.Database()
+        self.userThreads={}
+        self.userTokens={}
+        allUsers=self.database.getAllUsers()
+        for user in allUsers:
+            urls={}
+            userDevices=self.database.getAllDevices(user)
+            for device in userDevices:
+                if device["type"] in  self.supportedDevices:
+                    for metric in self.supportedDevices[device["type"]]["metrics"]:
+                        urls[device["type"]+"-"+metric]["url"]=self.supportedDevices[device["type"][metric]["url"]]
+                        urls[device["type"]+"-"+metric]["header"]=self.supportedDevices[device["type"]["header"]]+device["token"]+"\"}"
+                        urls[device["type"]+"-"+metric]["updateTime"]=self.supportedDevices[device["type"][metric]["updateTime"]]
+                        urls[device["type"]+"-"+metric]["savingTime"]=self.supportedDevices[device["type"][metric]["savingTime"]]
+            self.userThreads[user]=myThread(self, urls,user)
+            self.userThreads[user].start()
 
-    '''
-    def getPollution(self, city):
-        aux=requests.get("http://api.airvisual.com/v2/city?city="+city+"&state=Aveiro&country=Portugal&key=grJBqR7PN3NjzTZnL")
-        jsondata=json.loads(aux.text)
-        return str(jsondata["data"]["current"]["pollution"])
-    '''
 
-    def getAverageHeartRate(self):
-        #FitBit
-        url="https://api.fitbit.com/1/user/-/activities/heart/date/today/1d.json"
-        header={"Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkRLMlgiLCJzdWIiOiI3Q05RV1oiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcmFjdCBybG9jIHJ3ZWkgcmhyIHJwcm8gcm51dCByc2xlIiwiZXhwIjoxNTUyMzUwMDk2LCJpYXQiOjE1NTIzMjEyOTZ9.hhFY1zZliDYP3Kb9Cfe1OdxUBpduk7vP37zET2r05HY"}
 
-        response=requests.get(url, headers=header)
-        jsondata=json.loads(response.text)
-        return str(jsondata["activities-heart"][0]["value"]["restingHeartRate"])
+        #urls["https://api.fitbit.com/1/user/-/activities/heart/date/today/1d.json"]=[{"Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkRLMlgiLCJzdWIiOiI3Q05RV1oiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJudXQgcnBybyByc2xlIiwiZXhwIjoxNTUzODAxNjA5LCJpYXQiOjE1NTM3NzI4MDl9.6_hSXgYG36430e-ZaRfcEYSzDezGJeaMF5R2PiSr4bk"}, 1]
+        #urls["http://api.foobot.io/v2/device/240D676D40002482/datapoint/10/last/0/"]=[{"Accept":"application/json;charset=UTF-8","X-API-KEY-TOKEN":"eyJhbGciOiJIUzI1NiJ9.eyJncmFudGVlIjoiam9hby5wQHVhLnB0IiwiaWF0IjoxNTUyMDY2Njc5LCJ2YWxpZGl0eSI6LTEsImp0aSI6IjRiNmY2NzhiLWJjNTYtNDYxNi1hYmMyLTRiNjlkMTNkMjUzOSIsInBlcm1pc3Npb25zIjpbInVzZXI6cmVhZCIsImRldmljZTpyZWFkIl0sInF1b3RhIjoyMDAsInJhdGVMaW1pdCI6NX0.aeLLsrhh1-DVXSwl-Z_qDx1Xbr9oIid1IKsOyGQxwqQ"},1]
 
-    def getHomePollution(self):
-        #FooBot
-        header={"Accept":"text/csv;charset=UTF-8","X-API-KEY-TOKEN":"eyJhbGciOiJIUzI1NiJ9.eyJncmFudGVlIjoiam9hby5wQHVhLnB0IiwiaWF0IjoxNTUyMDY2Njc5LCJ2YWxpZGl0eSI6LTEsImp0aSI6IjRiNmY2NzhiLWJjNTYtNDYxNi1hYmMyLTRiNjlkMTNkMjUzOSIsInBlcm1pc3Npb25zIjpbInVzZXI6cmVhZCIsImRldmljZTpyZWFkIl0sInF1b3RhIjoyMDAsInJhdGVMaW1pdCI6NX0.aeLLsrhh1-DVXSwl-Z_qDx1Xbr9oIid1IKsOyGQxwqQ"}
-        url="http://api.foobot.io/v2/device/240D676D40002482/datapoint/10/last/0/"
-        response=requests.get(url, headers=header)
-        return response.text
+    #REAL THING
 
-    def getPollutionGPS(self,lat,longi):
-        #https://api.breezometer.com/air-quality/v2/current-conditions?lat=40.6&lon=-8.6&key=407f495d1f744a8c86c700e5fe83a752&features=breezometer_aqi,local_aqi,health_recommendations,sources_and_effects,pollutants_concentrations,pollutants_aqi_information
-        #https://api.waqi.info/feed/geo:40.6;-8.6/?token=453f8f3898bd238302fe5f84e3526a90c5da9496
-        response=requests.get("https://api.breezometer.com/air-quality/v2/current-conditions?lat="+lat+"&lon="+longi+"&key=407f495d1f744a8c86c700e5fe83a752&features=breezometer_aqi,local_aqi,health_recommendations,sources_and_effects,pollutants_concentrations,pollutants_aqi_information")
-        jsondata=json.loads(response.text)
-        out={}
-        for k in jsondata["data"]["pollutants"]:
-            out[jsondata["data"]["pollutants"][k]["full_name"]]=jsondata["data"]["pollutants"][k]["aqi_information"]["baqi"]["aqi_display"]
+    def signup(self, data):
+        jsonData=json.loads(data.decode("UTF-8"))
+        try:
+            self.database.register(jsonData)
+            return json.dumps({"status":0 , "error":"Successfull operation."}).encode("UTF-8")
+        except Exception as e:
+            return  json.dumps({"status":1, "error":"Database internal error. "+str(e)}).encode("UTF-8")
 
-        out[jsondata["data"]["indexes"]["baqi"]["display_name"]]=jsondata["data"]["indexes"]["baqi"]["aqi_display"]
 
-        #self.database.insert(out, lat, longi, "teste1", "1234")
+    def signin(self, data):
+        min_char = 30
+        max_char = 40
+        allchar = string.ascii_letters + string.digits
+        token = "".join(choice(allchar) for x in range(randint(min_char, max_char)))
+        while token in self.userTokens:
+            token = "".join(choice(allchar) for x in range(randint(min_char, max_char)))
+        
 
-        return str(out)
+        jsonData=json.loads(data.decode("UTF-8"))
+        if self.database.verifyUser(jsonData):
+            self.userTokens[token]=jsonData["username"]
+            return json.dumps({"status":0 , "error":"Successfull operation.", "data":{"token":"\""+token+"\""}}).encode("UTF-8")
+        return json.dumps({"status":1, "error":"Incorrect username or password."}).encode("UTF-8")
+
+    def getAllDevices(self, token):
+        if token not in self.userTokens:
+            return  json.dumps({"status":1, "error":"Invalid Token."}).encode("UTF-8")
+
+        user=self.userTokens[token]
+        devices=self.database.getAllDevices(user)
+        return json.dumps({"status":0 , "error":"Successfull operation.", "data":devices}).encode("UTF-8")
+
+    def addDevice(self, token, data):
+        if token not in self.userTokens:
+            return  json.dumps({"status":1, "error":"Invalid Token."}).encode("UTF-8")
+
+        user=self.userTokens[token]
+        try:
+            jsonData=json.loads(data.decode("UTF-8"))
+            deviceToken=jsonData["token"]
+
+            self.database.addDevice(user, )
+            return json.dumps({"status":0 , "error":"Successfull operation."}).encode("UTF-8")
+        except Exception as e:
+            return  json.dumps({"status":1, "error":"Database internal error. "+str(e)}).encode("UTF-8")
+
+    def getData(self, token, function, datatype, start, end, interval):
+        if token not in self.userTokens:
+            return  json.dumps({"status":1, "error":"Invalid Token."}).encode("UTF-8")
+
+        user=self.userTokens[token]
+        try:
+            values=eval("self.database."+function)
+            return json.dumps({"status":0 , "error":"Successfull operation.", "data":values}).encode("UTF-8")
+        except Exception as e:
+            return  json.dumps({"status":1, "error":"Database internal error. "+str(e)}).encode("UTF-8")
+        return  json.dumps({"status":1, "error":"Bad combination of arguments. It can only be start+end, start+interval or end+interval"}).encode("UTF-8")
+                
+
+    def updateProfile(self, token, data):
+        if token not in self.userTokens:
+            return  json.dumps({"status":1, "error":"Invalid Token."}).encode("UTF-8")
+
+        user=self.userTokens[token]
+        try:
+            self.database.updateProfile(user, json.loads(data.decode("UTF-8")))
+            return json.dumps({"status":0 , "error":"Successfull operation."}).encode("UTF-8")
+        except Exception as e:
+            return  json.dumps({"status":1, "error":"Database internal error. "+str(e)}).encode("UTF-8")
+
+    def getProfile(self, token):
+        if token not in self.userTokens:
+            return  json.dumps({"status":1, "error":"Invalid Token."}).encode("UTF-8")
+
+        user=self.userTokens[token]
+        try:
+            profile=self.database.getProfile(user)
+            return json.dumps({"status":0 , "error":"Successfull operation.", "data": profile}).encode("UTF-8")
+        except Exception as e:
+            return  json.dumps({"status":1, "error":"Database internal error. "+str(e)}).encode("UTF-8")
+
+    def deleteProfile(self, token):
+        if token not in self.userTokens:
+            return  json.dumps({"status":1, "error":"Invalid Token."}).encode("UTF-8")
+
+        user=self.userTokens[token]
+        try:
+            self.database.deleteProfile(user)
+            return json.dumps({"status":0 , "error":"Successfull operation."}).encode("UTF-8")
+        except Exception as e:
+            return  json.dumps({"status":1, "error":"Database internal error. "+str(e)}).encode("UTF-8")
+
+    def getSupportedDevices(self):
+        try:
+            values=self.database.getSupportedDevices()
+            return json.dumps({"status":0 , "error":"Successfull operation.", "data":values}).encode("UTF-8")
+        except Exception as e:
+            return  json.dumps({"status":1, "error":"Database internal error. "+str(e)}).encode("UTF-8")
+
+    def userGPSCoordinates(self, token, data):
+        if token not in self.userTokens:
+            return  json.dumps({"status":1, "error":"Invalid Token."}).encode("UTF-8")
+
+        user=self.userTokens[token]
+        jsonData=json.loads(data.decode("UTF-8"))
+        lat=jsonData["latitude"]
+        longi=jsonData["longitude"]
+        response=requests.get("https://api.waqi.info/feed/geo:"+lat+";"+longi+"/?token=453f8f3898bd238302fe5f84e3526a90c5da9496")
+        response=json.loads(response.text)
+        normalData=[[metric, reponse["data"]["iaqi"][metric]["v"]]for metric in reponse["data"]["iaqi"]]
+        normalData+=["aqi", response["data"]["aqi"]]
+        try:
+            self.database.insert("Environment", normalData, user)
+            return json.dumps({"status":0 , "error":"Successfull operation."}).encode("UTF-8")
+        except Exception as e:
+            return  json.dumps({"status":1, "error":"Database internal error. "+str(e)}).encode("UTF-8")    
+
+
+
+    def normalizeData(self, typeData, data):
+        jsonData=json.loads(data)
+        types=typeData.split("-")
+        return eval(self.supportedDevices[types[0]]["metrics"][types[1]]["getData"])
+
+    def process(self, typeData, data, user):
+        jsonData=json.loads(data)
+        types=typeData.split("-")
+        if eval(self.supportedDevices[types[0]]["metrics"][types[1]]["saveCondition"]):
+            self.save(typeData, data, user)
+
+    
+    def save(self, typeData, data, user):
+        normalData=self.normalizeData(typeData, data)
+        try:
+            self.database.insert(typeData, normalData, user)
+        except Exception as e:
+            raise e
+
+
+    def end(self):
+        for k in self.userThreads:
+            self.userThreads[k].end()
+        return ""
+
     
 
-
-#TODO finish thread, run not complete
 class myThread (threading.Thread):
-    def __init__(self, time_interval, url, header=None):
+    def __init__(self, processor, urls, user):
+        #urls= {type:{url: ...,header: ..., updatingTime: ..., savingTime: ...]}
         threading.Thread.__init__(self)
-        self.header=header
-        self.url=url
-        self.time_interval=time_interval
+        self.processor=processor
+        self.user=user
+        self.urls=urls
+        self.deltaTimes=[(timedelta(minutes=urls[metric]["updatingTime"]), timedelta(minutes=urls[metric]["savingTime"]), metric) for metric in urls]
+        self.running=True
 
     def run(self):
-        old_time=datetime.now()
-        while True:
-            if datetime.now()-timedelta(minutes=self.time_interval)>old_time:
-                response=requests.get(self.url, headers=self.header)
+        old_times=[(datetime.now(),datetime.now()) for x in range(len(self.urls))]
+        print("started")
+        while self.running:
+            updating=[datetime.now()-old[0] > delta[0] for old,delta in zip(old_times, self.deltaTimes)]
+            if any(updating):
+                for i,v in enumerate(updating):
+                    if v:
+                        old_times[i][0]=datetime.now()
+                        response=requests.get(self.urls[self.deltaTimes[i]]["url"], headers=self.urls[self.deltaTimes[i]]["header"])
+                        self.processor.process(response.text, self.user)
+            saving=[datetime.now()-old[1] > delta[1] for old,delta in zip(old_times, self.deltaTimes)]
+            if any(saving):
+                for i,v in enumerate(saving):
+                    if v:
+                        old_times[i][1]=datetime.now()
+                        response=requests.get(self.urls[self.deltaTimes[i]]["url"], headers=self.urls[self.deltaTimes[i]]["header"])
+                        self.processor.save(response.text, self.user)
+        print("ended")
       
+    def end(self):
+        self.running=False
 
 
-#thread1 = myThread(1, "Thread-1", 1)
-#thread1.start()
+
+
+
+'''
+def getAverageHeartRate(self):
+    #FitBit
+    url="https://api.fitbit.com/1/user/-/activities/heart/date/today/1d.json"
+    header={"Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkRLMlgiLCJzdWIiOiI3Q05RV1oiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJudXQgcnBybyByc2xlIiwiZXhwIjoxNTUzODAxNjA5LCJpYXQiOjE1NTM3NzI4MDl9.6_hSXgYG36430e-ZaRfcEYSzDezGJeaMF5R2PiSr4bk"}
+
+    response=requests.get(url, headers=header)
+    jsondata=json.loads(response.text)
+    return str(jsondata["activities-heart"][0]["value"]["restingHeartRate"])
+
+def getHomePollution(self):
+    #FooBot
+    header={"Accept":"application/json;charset=UTF-8","X-API-KEY-TOKEN":"eyJhbGciOiJIUzI1NiJ9.eyJncmFudGVlIjoiam9hby5wQHVhLnB0IiwiaWF0IjoxNTUyMDY2Njc5LCJ2YWxpZGl0eSI6LTEsImp0aSI6IjRiNmY2NzhiLWJjNTYtNDYxNi1hYmMyLTRiNjlkMTNkMjUzOSIsInBlcm1pc3Npb25zIjpbInVzZXI6cmVhZCIsImRldmljZTpyZWFkIl0sInF1b3RhIjoyMDAsInJhdGVMaW1pdCI6NX0.aeLLsrhh1-DVXSwl-Z_qDx1Xbr9oIid1IKsOyGQxwqQ"}
+    url="http://api.foobot.io/v2/device/240D676D40002482/datapoint/10/last/0/"
+    response=requests.get(url, headers=header)
+    return response.text
+
+def getPollutionGPS(self,lat,longi):
+    #https://api.breezometer.com/air-quality/v2/current-conditions?lat=40.6&lon=-8.6&key=407f495d1f744a8c86c700e5fe83a752&features=breezometer_aqi,local_aqi,health_recommendations,sources_and_effects,pollutants_concentrations,pollutants_aqi_information
+    #https://api.waqi.info/feed/geo:40.6;-8.6/?token=453f8f3898bd238302fe5f84e3526a90c5da9496
+    #http://api.openweathermap.org/data/2.5/weather?lat=40&lon=8&appid=a5168e6057120df470e1dcd0b02d9b38
+    response=requests.get("https://api.breezometer.com/air-quality/v2/current-conditions?lat="+lat+"&lon="+longi+"&key=407f495d1f744a8c86c700e5fe83a752&features=breezometer_aqi,local_aqi,health_recommendations,sources_and_effects,pollutants_concentrations,pollutants_aqi_information")
+    jsondata=json.loads(response.text)
+    out={}
+    for k in jsondata["data"]["pollutants"]:
+        out[jsondata["data"]["pollutants"][k]["full_name"]]=jsondata["data"]["pollutants"][k]["aqi_information"]["baqi"]["aqi_display"]
+
+    out[jsondata["data"]["indexes"]["baqi"]["display_name"]]=jsondata["data"]["indexes"]["baqi"]["aqi_display"]
+
+    self.database.insert(out, lat, longi, "teste1", "1234")
+    return str(out)
+
+def teste(self, data):
+    print(data)
+    return json.dumps({"resposta":"sim"}).encode("UTF-8")
+
+
+refresh FITBIT Token
+
+assume response
+
+jsonData=json.loads(response.text)
+if jsonData["success"] == False and jsonData[errors][0][errorType]=="expired_token":
+    refresh
+
+
+
+header={"Authorization": "Basic MjJESzJYOjUyNDRmZjEwNzliMTZlODQ4ZGE1YWI1NTc3ZDg5YzVl", "Content-Type": "application/x-www-form-urlencoded"}
+data={"grant_type":"refresh_token", "refresh_token": "e1bbbf6729924395bf5e47f98cbc090cbdea5b8701e6161c53250ca697f55397"}
+a=requests.post("https://api.fitbit.com/oauth2/token", headers=header, data=data)
+print(a.text)
+
+'''
--- Server/REST.py
+++ Server/REST.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 import json
 from Processor import Processor
 from flask_cors import CORS
-
+import ssl
 '''
 Component responsable for defining and supporting the interface to the system.
 '''
@@ -11,6 +11,9 @@ app = Flask(__name__)
 CORS(app)
 processor=Processor()
 
+
+#TESTS
+
 @app.route('/pollution/lat=<string:lat>/long=<string:longi>', methods = ['GET'])
 def pollution(lat, longi):
     return processor.getPollutionGPS(lat,longi)
@@ -18,3 +21,122 @@ def pollution(lat, longi):
 @app.route('/heartRate', methods = ['GET'])
 def heartRate():
     return processor.getAverageHeartRate()
+
+@app.route('/teste', methods = ['GET','POST'])
+def teste():
+    return processor.teste(request.data)
+
+@app.route('/end', methods = ['GET'])
+def end():
+    return processor.end()
+
+
+
+
+#REAL THING
+
+@app.route('/signup', methods = ['POST'])
+def signup():
+    return processor.signup(request.data)
+
+@app.route('/signin', methods = ['POST'])
+def signin():
+    return processor.signin(request.data)
+
+@app.route('/devices', methods = ['GET', 'POST'])
+def devices():
+    userToken=request.headers["AuthToken"]
+    if request.method == 'GET':
+        return processor.getAllDevices(userToken)
+    else:
+        return processor.addDevice(userToken, request.data)
+
+@app.route('/environment', endpoint="environment", methods = ['GET'])
+@app.route('/environment/<string:datatype>', endpoint="environmentSpecific", methods = ['GET'])
+@app.route('/healthstatus', endpoint="healthStatus", methods = ['GET'])
+@app.route('/healthstatus/<string:datatype>', endpoint="healthStatusSpecific", methods = ['GET'])
+@app.route('/personalstatus', endpoint="personalStatus", methods = ['GET'])
+@app.route('/download', endpoint="download",methods = ['GET'])
+def getData(datatype=None):
+    userToken=request.headers["AuthToken"]
+    start=request.args.get('start', default="*", type=str)
+    end=request.args.get('end', default="*", type=str)
+    interval=request.args.get('interval', default="*", type=str)
+    if start=="*" and end=="*" and interval=="*":
+        if request.endpoint=="environment":
+            function="getCurrentEnvironment(user)"
+        elif request.endpoint=="environmentSpecific":
+            function="getCurrentEnvironmentSpecific(user, datatype)"
+        elif request.endpoint=="healthStatus":
+            function="getCurrentHealthStatus(user)"
+        elif request.endpoint=="healthStatusSpecific":
+            function="getCurrentHealthStatusSpecific(user, datatype)"
+        elif request.endpoint=="personalStatus":
+            function="getCurrentPersonalStatus(user)"
+        elif request.endpoint=="download":
+            function="getAllData()"
+    elif end=="*":
+        if request.endpoint=="environment":
+            function="getEnvironmentStartInterval(user,start, interval)"
+        elif request.endpoint=="environmentSpecific":
+            function="getEnvironmentSpecificStartInterval(user,datatype, start, interval)"
+        elif request.endpoint=="healthStatus":
+            function="getHealthStatusStartInterval(user,start, interval)"
+        elif request.endpoint=="healthStatusSpecific":
+            function="getHealthStatusSpecificStartInterval(user,datatype, start, interval)"
+        elif request.endpoint=="personalStatus":
+            function="getPersonalStatusStartInterval(user,start, interval)"
+            elif request.endpoint=="download":
+            function="getAllDataStartInterval(user, start, interval)"
+    elif interval=="*":
+        if request.endpoint=="environment":
+            function="getEnvironmentStartEnd(user,start, end)"
+        elif request.endpoint=="environmentSpecific":
+            function="getEnvironmentSpecificStartEnd(user, datatype, start, end)"
+        elif request.endpoint=="healthStatus":
+            function="getHealthStatusStartEnd(user,start, end)"
+        elif request.endpoint=="healthStatusSpecific":
+            function="getHealthStatusSpecificStartEnd(user, datatype, start, end)"
+        elif request.endpoint=="personalStatus":
+            function="getPersonalStatusStartEnd(user,start, end)"
+        elif request.endpoint=="download":
+            function="getAllDataStartEnd(user,start, end)"
+    elif start=="*":
+        if request.endpoint=="environment":
+            function="getEnvironmentEndInterval(user,end, interval)"
+        elif request.endpoint=="environmentSpecific":
+            function="getEnvironmentSpecificEndInterval(user, datatype, end, interval)"
+        elif request.endpoint=="healthStatus":
+            function="getHealthStatusEndInterval(user,end, interval)"
+        elif request.endpoint=="healthStatusSpecific":
+            function="getHealthStatusSpecificEndInterval(user, datatype, end, interval)"
+        elif request.endpoint=="personalStatus":
+            function="getPersonalStatusEndInterval(user,end, interval)"
+        elif request.endpoint=="download":
+            function="getAllDataEndInterval(user,end, interval)"
+
+    return processor.getData(userToken, function, datatype, start, end, interval)
+
+@app.route('/profile', methods = ['GET', 'POST', 'DELETE'])
+def profile():
+    userToken=request.headers["AuthToken"]
+    if request.method == 'POST':
+        return processor.updateProfile(userToken, request.data)
+    elif request.method == 'GET':
+        return processor.getProfile(userToken)
+    else:
+        return processor.deleteProfile(userToken)
+
+@app.route('/supportedDevices', methods = ['GET'])
+def supportedDevices():
+    return processor.getSupportedDevices()
+
+
+@app.route('/gps', methods = ['POST'])
+def userGPSCoordinates():
+    userToken=request.headers["AuthToken"]
+    return processor.userGPSCoordinates(userToken, request.data)
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain("MyRootCA.crt", "MyRootCA.key")
+app.run(host='0.0.0.0',port='5000', debug = False/True, ssl_context=context)
