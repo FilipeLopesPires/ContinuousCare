203ddaba0e6b59785ff969b5b3b19daf89eeb919
aspedrosa <aspedrosa@ua.pt>
2019-05-06 19:42:12 +0100

    Beginning of permissions page

---
+++ Client/components/boxes/PermissionsBox.vue
@@ -0,0 +1,193 @@
+<template>
+    <table class="w-100" :id="title.toLowerCase()">
+        <thead>
+            <tr>
+                <th>Username</th>
+                <th>Full Name</th>
+                <th v-if="user_type === 'medic'">Health Number</th>
+                <th v-if="user_type === 'client'">Company</th>
+                <th v-if="title === 'Pending'">Duration</th>
+                <th v-else-if="title === 'Accepted' || title === 'Active'">Time Left</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="permission in permissions" :key="permission.username">
+                <td>{{ permission.username }}</td>
+                <td>{{ permission.full_name }}</td>
+                <td v-if="user_type === 'medic'">{{ permission.health_number }}</td>
+                <td v-if="user_type === 'client'">{{ permission.company }}</td>
+                <td>{{ permission.duration }}</td>
+
+                <td>
+                    <div v-if="title === 'Pending'">
+                        <div v-if="user_type === 'medic'">
+                            <a @click="remove_pending(permission.username)" class="genric-btn danger circle"><i class="fa fa-trash" aria-hidden="true"></i></a>
+                        </div>
+                        <div v-if="user_type === 'client'">
+                            <a @click="accept_permission(permission.username)" class="genric-btn success circle">
+                                <i class="fa fa-check" aria-hidden="true"></i>
+                            </a>
+                            <a @click="reject_permission(permission.username)" class="genric-btn danger circle">
+                                <i class="fa fa-times" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div v-else-if="title === 'Accepted'">
+                        <div v-if="user_type === 'medic'">
+                            <a class="genric-btn success circle"><i class="fa fa-play" aria-hidden="true"></i></a>
+                        </div>
+                        <div v-if="user_type === 'client'">
+                            <a @click="remove_accepted(permission.username)" class="genric-btn danger circle"><i class="fa fa-trash"></i></a>
+                        </div>
+                    </div>
+                    <div v-else-if="title === 'Active'">
+                        <div v-if="user_type === 'medic'">
+                            <a @click="remove_active(permission.username)" class="genric-btn warning circle"><i class="fa fa-pause"></i></a>
+                        </div>
+                        <div v-if="user_type === 'client'">
+                            <a @click="pause_active(permission.username)" class="genric-btn danger circle"><i class="fa fa-trash"></i></a>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+
+export default {
+    name: 'PermissionsBox',
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        user_type: {
+            type: String,
+            required: true
+        }
+    },
+    data() {
+        return {
+            permissions: [
+                {
+                    username: "aspedrosa",
+                    full_name: "André Pedrosa",
+                    company: "Hospital Leiria",
+                    health_number: 111111111,
+                    duration: 10
+                },
+                {
+                    username: "zonnax",
+                    full_name: "Rodrigo Loureiro",
+                    company: "Hospital Leiria",
+                    health_number: 111111112,
+                    duration: 10
+                }
+            ]
+        }
+    },
+    methods: {
+        /**
+         * 
+         */
+        async accept_permission(medic_username) {
+            return await this.$axios.$post("", {
+
+            })
+            .then(res => {
+
+            })
+            .catch(e => {
+
+            })
+        },
+
+        /**
+         * 
+         */
+        async reject_permission(medic_username) {
+            return await this.$axios.$post("", {
+
+            })
+            .then(res => {
+
+            })
+            .catch(e => {
+
+            })
+        },
+
+        /**
+         * 
+         */
+        async remove_pending(client_username) {
+            return await this.$axios.$post("", {
+
+            })
+            .then(res => {
+
+            })
+            .catch(e => {
+
+            })
+        },
+
+        /**
+         * 
+         */
+        async remove_accepted(medic_username) {
+            return await this.$axios.$post("", {
+
+            })
+            .then(res => {
+
+            })
+            .catch(e => {
+
+            })
+        },
+
+        /**
+         * 
+         */
+        async remove_active(medic_username) {
+            return await this.$axios.$post("", {
+
+            })
+            .then(res => {
+
+            })
+            .catch(e => {
+
+            })
+        },
+
+        /**
+         * 
+         */
+        async pause_active(client_username) {
+            return await this.$axios.$post("", {
+
+            })
+            .then(res => {
+
+            })
+            .catch(e => {
+
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+.custom_button {
+    width: 50px;
+}
+.padding {
+    margin-bottom: 10px;
+    margin-top: 10px;
+}
+</style>
--- Client/pages/patients/index.vue
+++ Client/pages/patients/index.vue
@@ -6,6 +6,58 @@
 
             <!--================ Banner Area =================-->
             <PageBanner parent_page="Home" page="Patients" />
+
+            <!--================ Permissions Boxes Area ============-->
+            <b-container class="padding">
+                <b-row>
+                    <!-- =============================== Modal =======================-->
+                    <b-modal ref="request_grant_permission_modal" hide-footer hide-header>
+                        <div v-if="user_type === 'medic'">
+                            <h3  class="title_color text-center" >Request Permission</h3>
+                            <h5>Patient Username:</h5>
+                            <input id="req_gra_client_username" type="text" class="single-input"> 
+                            <h5>Patient Health Number:</h5>
+                            <input id="req_gra_health_number" type="number" class="single-input"> 
+                        </div>
+                        <div v-else-if="user_type === 'client'">
+                            <h3  class="title_color text-center" >Grant Permission</h3>
+                            <div class="mt-10"> 
+                                <h5>Medic Username:</h5>
+                                <input id="req_gra_medic_username" type="text" class="single-input"> 
+                            </div>
+                        </div>
+                        <h5>Duration:</h5>
+                        <input id="req_gra_duration" type="number" class="single-input">
+                        <div class="mt-10 row justify-content-center d-flex align-items-center">
+                            <div class="row">
+                                <button @click="close_modal" data-dismiss="modal" class="genric-btn danger circle text-uppercase ml-10 mr-10" type="button" >Cancel</button>
+                                <button @click="request_grant_permission" class="genric-btn info circle text-uppercase ml-10 mr-10" type="submit" >Add</button>
+                            </div>
+                        </div>
+                    </b-modal>
+                    <a
+                        class="genric-btn success circle custom_button mb-20"
+                        @click="$refs['request_grant_permission_modal'].show()">
+                        <span v-if="user_type === 'medic'"><i class="fa fa-plus"></i> Request Permission</span>
+                        <span v-else-if="user_type === 'client'"><i class="fa fa-plus"></i> Grant Permission</span>
+                    </a>
+                </b-row>
+                <b-row>
+                    <b-card no-body class="w-100">
+                        <b-tabs card justified>
+                            <b-tab title="Pending">
+                                <PermissionsBox title="Pending" :user_type="user_type" />
+                            </b-tab>
+                            <b-tab title="Accepted">
+                                <PermissionsBox title="Accepted" :user_type="user_type" />
+                            </b-tab>
+                            <b-tab title="Active">
+                                <PermissionsBox title="Active" :user_type="user_type" />
+                            </b-tab>
+                        </b-tabs>
+                    </b-card>
+                </b-row>
+            </b-container>
             
             <!--================ Footer Area =================-->
             <PageFooter />
@@ -16,18 +68,74 @@
 </template>
 
 <script>
+import Vue from 'vue'
+import VModal from 'vue-js-modal/dist/ssr.index'
+Vue.use(VModal)
+
+import PermissionsBox from '@/components/boxes/PermissionsBox.vue'
 
 export default {
     middleware: ['check-log', 'log'],
     components: {
-        
+        PermissionsBox
     },
     head: {
         title: "Patients"
+    },
+    data() {
+        return {
+            user_type: "client"
+        }
+    },
+    methods: {
+        /**
+         * 
+         */
+        async request_grant_permission() {
+            let username, health_number, duration;
+
+            if (this.user_type === "medic") {
+                username = $("#req_gra_client_username").val();
+                health_number = $("#req_gra_health_number").val();
+            } else if (this.user_type === "client")
+                username = $("#req_gra_medic_username").val();
+
+            duration = $("#req_gra_duration").val();
+
+            return await this.$axios.$post("", {
+
+            })
+            .then(res => {
+
+            })
+            .catch(e => {
+
+            })
+        },
+
+        /**
+         * Clears all inputs on the modal and closes it
+         */
+        close_modal() {
+            $("#req_gra_client_username").val("");
+            $("#req_gra_health_number").val("");
+            $("#req_gra_medic_username").val("");
+            $("#req_gra_duration").val("");
+            this.$refs["request_grant_permission_modal"].hide();
+        }
     }
 }
 </script>
 
 <style scoped>
+.padding {
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+
+.column_border {
+    border-style: solid;
+    border-width: 1px;
+}
 
 </style>
